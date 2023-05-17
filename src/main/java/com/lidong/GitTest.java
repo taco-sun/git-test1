ad0cf3af1836bd85e921ece709bd14c0232c6c91
LiSong <LiSong@qq.com>
2023-05-17 22:47:30 +0800

    first ff commit

--- src/main/java/com/lidong/GitTest.java
+++ src/main/java/com/lidong/GitTest.java
@@ -5,5 +5,8 @@ public class GitTest {
         System.out.println("hello Git");
         System.out.println("hello Git2");
         System.out.println("hello Git3");
+        System.out.println("hello Git4");
+
+
     }
 }
