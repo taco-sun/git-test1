7ddf6213168226625a60cfb6ba7dc529c1ce81ae
LiSong <LiSong@qq.com>
2023-05-18 11:55:14 +0800

    four commit

--- src/main/java/com/lidong/GitTest.java
+++ src/main/java/com/lidong/GitTest.java
@@ -6,6 +6,8 @@ public class GitTest {
         System.out.println("hello Git2");
         System.out.println("hello Git3");
         System.out.println("hello Git4");
+        System.out.println("push Git4");
+
 
 
     }
